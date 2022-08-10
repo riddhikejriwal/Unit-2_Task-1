d53994ed136912b660c008639c524d9d6f4a0c00
riddhikejriwal <88707108+riddhikejriwal@users.noreply.github.com>
2022-08-10 23:22:28 +0530

    Update threadrunnable5.java

--- threadrunnable5.java
+++ threadrunnable5.java
@@ -1,1 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
 
+class threadrunnable5 implements Runnable{
+    public void run(){
+        System.out.println("Hello India: "+Thread.currentThread().getName());
+    }
+    public static void main(String[] args) {
+        for(int i=0;i<5;i++){
+        	Thread thread=new Thread(new threadrunnable5());
+            thread.start();
+        }
+    }
+}
